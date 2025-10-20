fbd81192b6edfd82f2fe36ae95c3e55ceaae5dfd
Joshua Liew <liqshann@gmail.com>
2025-10-20 15:26:24 +0900

    feat(boot): add ascii art

---
+++ boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# TODO: add some ascii art for personality
+ascii_art='
+                     _                          
+  __  ______        (_)___        ___  ____ ___ 
+ / / / / __ \______/ / __ \______/ _ \/ __ `__ \
+/ /_/ / /_/ /_____/ / / / /_____/  __/ / / / / /
+\__,_/ .___/     /_/_/ /_/      \___/_/ /_/ /_/ 
+    /_/                                         
+'
+
+echo -e "$ascii_art"
+echo "=> Installing your eduroam infrastructure up-in-em!"
+echo -e "\nTo abort installation: CTRL+C"
+echo -e "\nBegin installation..."
