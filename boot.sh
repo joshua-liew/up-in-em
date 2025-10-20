9f2c1c9ce83a307a3634c5ce8f51bb83d26426aa
Joshua Liew <liqshann@gmail.com>
2025-10-20 16:10:52 +0900

    feat(boot): check previous installation

--- boot.sh
+++ boot.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-# TODO: add some ascii art for personality
 ascii_art='
                      _                          
   __  ______        (_)___        ___  ____ ___ 
@@ -14,5 +13,35 @@ ascii_art='
 
 echo -e "$ascii_art"
 echo "=> Installing your eduroam infrastructure up-in-em!"
-echo -e "\nTo abort installation: CTRL+C"
-echo -e "\nBegin installation..."
+echo "To abort installation: CTRL+C"
+echo "Begin installation..."
+
+echo -e "\nChecking for previous installations of up-in-em..."
+UPINEM_PATH="$HOME/.local/share/upinem"
+if [ -d "$UPINEM_PATH" ]; then
+    echo "Previous installation of up-in-em detected at '$UPINEM_PATH'."
+    echo "To install up-in-em, you must uninstall any previous version."
+    while true; do
+        read -p "Enter 'y' to uninstall the previous installation: " var
+        case $var in
+            y)
+                echo "Uninstalling previous version..."
+                # TODO: run the uninstall script
+                break
+                ;;
+            n | N)
+                echo "Installation aborted."
+                break
+                ;;
+            *)
+                echo "Invalid input. Please enter 'y' or 'n/N'."
+                ;;
+        esac
+    done
+fi
+
+# TODO: run uninstall.sh, delete previous installation, reinstall
+#
+# TODO: check os version - only work with Ubuntu 24.04+
+#
+# TODO: make stdout more colorful!
