2b25c4f2e5d7cb9b1e6b79f042f52ccb66a1ab2e
Joshua Liew <liqshann@gmail.com>
2025-11-05 00:23:49 +0900

    refactor: install - remove redundant parts

--- install/install.sh
+++ install/install.sh
@@ -29,21 +29,6 @@ source ${INSTALL_DIR}/freeradius/install-freeradius.sh
 source ${INSTALL_DIR}/vault/_config-vault.sh
 source ${INSTALL_DIR}/vault/install-vault.sh
 
-echo "* Configuring vault..."
-# https://developer.hashicorp.com/vault/docs/get-vault/install-binary
-# Step 1: Configure the environment
-echo "* Configuring environment for vault..."
-export VAULT_DATA=/opt/vault/data
-export VAULT_CONFIG=/etc/vault.d
-sudo mv $(which vault) /usr/bin
-sudo setcap cap_ipc_lock=+ep $(readlink -f $(which vault))
-sudo mkdir -p ${VAULT_DATA}
-sudo mkdir -p ${VAULT_CONFIG}
-# Step 2: Configure user permissions
-echo "* Configuring user permissions for vault..."
-sudo useradd --system --home ${VAULT_DATA} --shell /sbin/nologin vault
-sudo chown vault:vault ${VAULT_DATA}
-sudo chmod -R 750 ${VAULT_DATA}
 
 
 # TODO: install CLI tool
