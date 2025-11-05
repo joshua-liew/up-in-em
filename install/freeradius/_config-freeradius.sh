131e42c41eddeab51c665440c47e4f338e5e031c
Joshua Liew <liqshann@gmail.com>
2025-11-05 18:10:39 +0900

    feat(install): freeradius - custom build according to FHS & best practices

--- install/freeradius/_config-freeradius.sh
+++ install/freeradius/_config-freeradius.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
 export FRAD_REPO_DIR=$HOME/.local/share/freeradius-server
-export FRAD_DIR=$HOME/freeradius
+# Add according to the FHS
+export FRAD_DIR=/usr/local
+export FRAD_LOCALSTATE_DIR=/var
+# Custom configuration: add to the config directory
 export FRAD_CONFIG_DIR=${CONFIG_DIR}/raddb
+# From ./configure --help:
+# Fine tuning of the installation directories:
+#  --bindir=DIR            user executables [EPREFIX/bin]
+#  --sbindir=DIR           system admin executables [EPREFIX/sbin]
+#  --libexecdir=DIR        program executables [EPREFIX/libexec]
+#  --sysconfdir=DIR        read-only single-machine data [PREFIX/etc]
+#  --sharedstatedir=DIR    modifiable architecture-independent data [PREFIX/com]
+#  --localstatedir=DIR     modifiable single-machine data [PREFIX/var]
+#  --runstatedir=DIR       modifiable per-process data [LOCALSTATEDIR/run]
+#  --libdir=DIR            object code libraries [EPREFIX/lib]
+#  --includedir=DIR        C header files [PREFIX/include]
+#  --oldincludedir=DIR     C header files for non-gcc [/usr/include]
+#  --datarootdir=DIR       read-only arch.-independent data root [PREFIX/share]
+#  --datadir=DIR           read-only architecture-independent data [DATAROOTDIR]
+#  --infodir=DIR           info documentation [DATAROOTDIR/info]
+#  --localedir=DIR         locale-dependent data [DATAROOTDIR/locale]
+#  --mandir=DIR            man documentation [DATAROOTDIR/man]
+#  --docdir=DIR            documentation root [DATAROOTDIR/doc/freeradius]
+#  --htmldir=DIR           html documentation [DOCDIR]
+#  --dvidir=DIR            dvi documentation [DOCDIR]
+#  --pdfdir=DIR            pdf documentation [DOCDIR]
+#  --psdir=DIR             ps documentation [DOCDIR]
+#  ...
+#  Optional Packages:
+#  --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
+#  --without-PACKAGE       do not use PACKAGE (same as --with-PACKAGE=no)
+#  --with-docdir=DIR       directory for documentation DATADIR/doc/freeradius
+#  --with-logdir=DIR       directory for logfiles LOCALSTATEDIR/log/radius
+#  --with-radacctdir=DIR   directory for detail files LOGDIR/radacct
+#  --with-raddbdir=DIR     directory for config files SYSCONFDIR/raddb
+#  --with-dictdir=DIR      directory for dictionary files DATAROOTDIR/freeradius
--- install/freeradius/install-freeradius.sh
+++ install/freeradius/install-freeradius.sh
@@ -31,7 +31,7 @@ echo "* [ START ] Building freeradius..."
 git checkout release_3_2_8 >/dev/null
 # Step 1: Configure the installation
 echo "* Configuring the build process..."
-./configure --prefix=${FRAD_DIR} --with-raddbdir=${FRAD_CONFIG_DIR} --with-modules='rlm_eap_tls,rlm_rest,rlm_sql_sqlite' --enable-strict-dependencies --enable-werror --enable-reproducible-builds --disable-option-checking --without-experimental-modules --cache-file=/dev/null --srcdir=. --without-rlm_eap_tnc --without-rlm_eap_ikev2 --without-rlm_krb5 --without-rlm_ldap --without-rlm_digest --without-rlm_dynamic_clients --without-rlm_eap_fast --without-rlm_eap_gc --without-rlm_eap_md5 --without-rlm_eap_sim --without-rlm_eap_teap --without-rlm_opendirectory --without-rlm_eap_securid --without-rlm_smsotp --without-rlm_soh --without-rlm_totp --without-rlm_wimax --without-rlm_yubikey --without-rlm_cache_rbtree --without-rlm_couchbase --without-rlm_dpsk --without-rlm_kafka --without-rlm_ldap --without-rlm_sql_db2 --without-rlm_sql_firebird --without-rlm_sql_freetds --without-rlm_sql_iodbc --without-rlm_sql_mongo --without-rlm_sql_mysql --without-rlm_sql_oracle --without-rlm_sql_postgresql --without-rlm_sql_unixodbc --without-rlm_exec --without-rlm_perl --without-rlm_python --without-rlm_python3 --without-rlm_ruby --without-rlm_proxy_rate_limit --without-rlm_pam --without-rlm_unbound >/dev/null 2>&1
+./configure --prefix=${FRAD_DIR} --localstatedir=${FRAD_LOCALSTATE_DIR} --with-raddbdir=${FRAD_CONFIG_DIR} --with-modules='rlm_eap_tls,rlm_rest,rlm_sql_sqlite' --enable-strict-dependencies --enable-werror --enable-reproducible-builds --disable-option-checking --without-experimental-modules --cache-file=/dev/null --srcdir=. --without-rlm_eap_tnc --without-rlm_eap_ikev2 --without-rlm_krb5 --without-rlm_ldap --without-rlm_digest --without-rlm_dynamic_clients --without-rlm_eap_fast --without-rlm_eap_gc --without-rlm_eap_md5 --without-rlm_eap_sim --without-rlm_eap_teap --without-rlm_opendirectory --without-rlm_eap_securid --without-rlm_smsotp --without-rlm_soh --without-rlm_totp --without-rlm_wimax --without-rlm_yubikey --without-rlm_cache_rbtree --without-rlm_couchbase --without-rlm_dpsk --without-rlm_kafka --without-rlm_ldap --without-rlm_sql_db2 --without-rlm_sql_firebird --without-rlm_sql_freetds --without-rlm_sql_iodbc --without-rlm_sql_mongo --without-rlm_sql_mysql --without-rlm_sql_oracle --without-rlm_sql_postgresql --without-rlm_sql_unixodbc --without-rlm_exec --without-rlm_perl --without-rlm_python --without-rlm_python3 --without-rlm_ruby --without-rlm_proxy_rate_limit --without-rlm_pam --without-rlm_unbound >/dev/null 2>&1
 # Step 2: Make-ing the installation
 echo "* Bootstrapping the build process..."
 make >/dev/null 2>&1
@@ -48,11 +48,7 @@ echo "* [ SUCCESS ] Build process for freeradius is complete!"
 cd - >/dev/null
 # Step 1: Configure the environment
 echo "* Configuring environment for freeradius..."
-sudo mv ${FRAD_DIR}/bin/* /usr/local/sbin
-sudo mv ${FRAD_DIR}/sbin/* /usr/local/sbin
-sudo mv $(which radiusd) /usr/local/bin
+sudo ldconfig
 # Step 2: Configure user permissions
 echo "* Configuring user permissions for freeradius..."
 sudo useradd --system --home ${FRAD_DIR} --shell /sbin/nologin freeradius
-sudo usermod -aG freeradius $USER
-sudo chown freeradius:freeradius -R ${FRAD_CONFIG_DIR}
