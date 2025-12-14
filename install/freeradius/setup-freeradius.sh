6a0da3d6082b56ca8d51081c1127420f82bb9cdc
Joshua Liew <liqshann@gmail.com>
2025-12-14 22:38:35 +0900

    feat(install): radius setup - config setup

--- install/freeradius/setup-freeradius.sh
+++ install/freeradius/setup-freeradius.sh
@@ -7,9 +7,9 @@ echo "* Setting up freeradius..."
 # Prep
 # --------------------------------------------------------------
 
-sudo cp -p ${FRAD_CONFIG_DIR}/radiusd.conf ${FRAD_CONFIG_DIR}/radiusd.conf.orig
-sudo cp -p ${FRAD_CONFIG_DIR}/proxy.conf ${FRAD_CONFIG_DIR}/proxy.conf.orig
-sudo cp -p ${FRAD_CONFIG_DIR}/clients.conf ${FRAD_CONFIG_DIR}/clients.conf.orig
+sudo mv ${FRAD_CONFIG_DIR}/radiusd.conf ${FRAD_CONFIG_DIR}/radiusd.conf.orig
+sudo mv ${FRAD_CONFIG_DIR}/proxy.conf ${FRAD_CONFIG_DIR}/proxy.conf.orig
+sudo mv ${FRAD_CONFIG_DIR}/clients.conf ${FRAD_CONFIG_DIR}/clients.conf.orig
 sudo cp -pr ${FRAD_CONFIG_DIR}/mods-enabled ${FRAD_CONFIG_DIR}/mods-enabled.orig
 sudo cp -pr ${FRAD_CONFIG_DIR}/sites-enabled ${FRAD_CONFIG_DIR}/sites-enabled.orig
 sudo cp -pr ${FRAD_CONFIG_DIR}/certs ${FRAD_CONFIG_DIR}/certs.orig
@@ -28,3 +28,35 @@ source ${UPINEM_PATH}/config/freeradius/upinem.proxy.conf.sh >/dev/null
 source ${UPINEM_PATH}/config/freeradius/upinem.clients.conf.sh >/dev/null
 source ${UPINEM_PATH}/config/freeradius/mods.eap.sh >/dev/null
 source ${UPINEM_PATH}/config/freeradius/sites.eduroam.sh >/dev/null
+
+
+# --------------------------------------------------------------
+# Setup Configs
+# --------------------------------------------------------------
+
+# Move configs
+sudo cp radiusd.conf $FRAD_CONFIG_DIR
+sudo cp proxy.conf $FRAD_CONFIG_DIR
+sudo cp clients.conf $FRAD_CONFIG_DIR
+sudo cp eap ${FRAD_CONFIG_DIR}/mods-available
+sudo cp eduroam ${FRAD_CONFIG_DIR}/sites-available
+# Set privileges
+sudo chmod 640 ${FRAD_CONFIG_DIR}/radiusd.conf
+sudo chmod 640 ${FRAD_CONFIG_DIR}/proxy.conf
+sudo chmod 640 ${FRAD_CONFIG_DIR}/clients.conf
+sudo chmod 640 ${FRAD_CONFIG_DIR}/mods-available/eap
+sudo chmod 640 ${FRAD_CONFIG_DIR}/sites-available/eduroam
+
+# Enable modules
+source ${UPINEM_PATH}/config/freeradius/mods.txt.sh >/dev/null
+xargs -I {mod} \ 
+  sudo ln -sr /etc/freeradius/mods-available/{mod} \
+  /etc/freeradius/mods-enabled/{mod} < mods.txt
+# Enable virtual servers (sites)
+source ${UPINEM_PATH}/config/freeradius/sites.txt.sh >/dev/null
+xargs -I {site} \ 
+  sudo ln -sr /etc/freeradius/sites-available/{site} \
+  /etc/freeradius/sites-enabled/{site} < sites.txt
+
+# Set permissions
+sudo chown freeradius:freeradius -Rh $FRAD_CONFIG_DIR
