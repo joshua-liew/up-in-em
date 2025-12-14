1f2924b7201bd0a3faff5d8cf37d10014b97ce47
Joshua Liew <liqshann@gmail.com>
2025-12-14 18:42:51 +0900

    feat(install): radius setup - generate config

--- install/freeradius/setup-freeradius.sh
+++ install/freeradius/setup-freeradius.sh
@@ -19,4 +19,12 @@ sudo rm -rf ${FRAD_CONFIG_DIR}/sites-enabled/*
 sudo rm -rf ${FRAD_CONFIG_DIR}/certs/*
 
 
+# --------------------------------------------------------------
+# Generate Config(s)
+# --------------------------------------------------------------
 
+source ${UPINEM_PATH}/config/freeradius/upinem.radiusd.conf.sh >/dev/null
+source ${UPINEM_PATH}/config/freeradius/upinem.proxy.conf.sh >/dev/null
+source ${UPINEM_PATH}/config/freeradius/upinem.clients.conf.sh >/dev/null
+source ${UPINEM_PATH}/config/freeradius/mods.eap.sh >/dev/null
+source ${UPINEM_PATH}/config/freeradius/sites.eduroam.sh >/dev/null
